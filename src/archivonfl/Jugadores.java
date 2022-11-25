eabe0100c4b1b1becd504e6ed40e84f1fcba04aa
JuanMerino15 <juanrivmerino4@gmail.com>
2022-11-24 21:17:02 -0600

    Añadiendo jugadores

---
+++ src/archivonfl/Jugadores.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archivonfl;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author juan
+ */
+public class Jugadores {
+      private String name;
+       LinkedList<Equipos> Equipos;
+       
+       public Jugadores(String name){
+        this.name = name;
+        Equipos = new LinkedList<>();
+    }
+       
+     public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+   public void add(Equipos unEquipos){
+       this.Equipos.add(unEquipos);
+   }
+   
+   public void remove(Equipos unEquipos){
+       this.Equipos.remove(unEquipos);
+   }
+
+    public LinkedList<Equipos> getEquipos() {
+        return Equipos;
+    }
+    
+        public Equipos getEquipos(int e) {
+        return Equipos.get(e);
+    }
+        
+        
+    
+
+}
+
+    
+   
